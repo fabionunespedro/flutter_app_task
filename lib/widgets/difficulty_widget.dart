96be834fa5ba544ac3d4bae16cfd850e36d2ac3d
Fabio Nunes <fabio.pedro@live.com>
2025-03-06 16:41:23 -0300

    change StatefulWidget for StatelessWidget

--- lib/widgets/difficulty_widget.dart
+++ lib/widgets/difficulty_widget.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 
-class DifficultyWidget extends StatefulWidget {
+class DifficultyWidget extends StatelessWidget {
   const DifficultyWidget({super.key, required this.difficultyColors});
 
   final int difficultyColors;
 
   @override
-  State<DifficultyWidget> createState() => _DifficultyWidgetState();
-}
-
-class _DifficultyWidgetState extends State<DifficultyWidget> {
-  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Icon(Icons.star,
-            size: 18, color: (widget.difficultyColors >= 1) ? Colors.amber : Colors.amber[100]),
+            size: 18, color: (difficultyColors >= 1) ? Colors.amber : Colors.amber[100]),
         Icon(Icons.star,
-            size: 18, color: (widget.difficultyColors >= 2) ? Colors.amber : Colors.amber[100]),
+            size: 18, color: (difficultyColors >= 2) ? Colors.amber : Colors.amber[100]),
         Icon(Icons.star,
-            size: 18, color: (widget.difficultyColors >= 3) ? Colors.amber : Colors.amber[100]),
+            size: 18, color: (difficultyColors >= 3) ? Colors.amber : Colors.amber[100]),
         Icon(Icons.star,
-            size: 18, color: (widget.difficultyColors >= 4) ? Colors.amber : Colors.amber[100]),
+            size: 18, color: (difficultyColors >= 4) ? Colors.amber : Colors.amber[100]),
         Icon(Icons.star,
-            size: 18, color: (widget.difficultyColors >= 5) ? Colors.amber : Colors.amber[100]),
+            size: 18, color: (difficultyColors >= 5) ? Colors.amber : Colors.amber[100]),
       ],
     );
   }
